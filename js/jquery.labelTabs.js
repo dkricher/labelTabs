72fbb1d5d7d6ce535bceaada065b38002fa76e4f
Dan Richardson <dandrichardson@gmail.com>
2014-02-12 10:09:49 -0600

    Update jquery.labelTabs.js

--- js/jquery.labelTabs.js
+++ js/jquery.labelTabs.js
@@ -1,45 +1,34 @@
-/* 2014 Dan Richardson */
-
 (function ($) {
     $.fn.labelTabs = function () {
-        this.each(function () {
-            var $thislabel = $(this);
-            var lblForAttr = $thislabel.attr('for');
-            var $field = $('#' + lblForAttr);
-            if ($field.length === 0) { return; }
-            var lblWidth = $field.width();
-            var fldZindex = $field.css('zIndex');
-            if ($field.is('select')) {
-                $thislabel.addClass('selectLabel').css({ width: lblWidth - 5, 'z-index': fldZindex + 1 });
-            } else if (!$field.is('textarea')) {
-                $thislabel.css({ 'width': lblWidth, 'z-index': fldZindex + 1 });
-            }
-        });
         function lblFocus() {
             var $field = $(this);
-            var $fieldId = $field.attr('id');
-            var $thislabel = $('label[for="' + $fieldId + '"].labelTabs');
-            $thislabel.addClass('focus');
+            var $thislabel = $('label[for="' + $field.attr('id') + '"].labelTabs');
+            $thislabel.addClass('value-set');
+            $field.addClass('value-set');
+            if ($field.is('textarea')) {
+                $thislabel.animate({'margin-top': '15px'}, 500);
+            }
         }
         function lblBlur() {
             var $field = $(this);
-            var $fieldId = $field.attr('id');
-            var $thislabel = $('label[for="' + $fieldId + '"].labelTabs');
-            $thislabel.removeClass('focus');
-            if ($field.val() !== '') {
-                $thislabel.addClass('value-set');
-            } else {
+            var $thislabel = $('label[for="' + $field.attr('id') + '"].labelTabs');
+            if ($field.val() === '') {
                 $thislabel.removeClass('value-set');
+                $field.removeClass('value-set');
             }
         }
         $('input, select, textarea').each(function () {
             var $field = $(this);
-            var $fieldId = $field.attr('id');
-            var $thislabel = $('label[for="' + $fieldId + '"].labelTabs');
+            var $thislabel = $('label[for="' + $field.attr('id') + '"].labelTabs');
+            if ($field.is('textarea')) {
+                $thislabel.animate({ 'margin-top': '15px' }, 500);
+            }
             if ($field.val() !== '') {
                 $thislabel.addClass('value-set');
+                $field.addClass('value-set');
             } else {
                 $thislabel.removeClass('value-set');
+                $field.removeClass('value-set');
             }
         });
         $('input, select, textarea').focus(lblFocus).blur(lblBlur);
